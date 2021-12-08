e25898e04a558e2cbe16dfc8eba7e2760bbdd4ba
Sally Jiang <46103394+SallyDJiang@users.noreply.github.com>
2021-12-08 02:41:57 -0500

    Added DocStrings
    - Fixed point system
    - Fixed bug where game would crash

--- PyCross/createpicross.py
+++ PyCross/createpicross.py
@@ -6,11 +6,11 @@ from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 import random
 from PIL import Image
 
+# Set Style 
 style = 'default'
 plt.style.use(style)
 plt.rcParams['font.family'] = 'fantasy'
-
-
+ 
 # takes image given by user and rebinned the image 
 def img2bw(im, threshold = 50):
     '''
@@ -51,9 +51,9 @@ def load_image(img=None, window_sizex=20, window_sizey=20):
     ----------
     img: array_like
         image to be used  
-    window_sizex int
+    window_sizex: int
         size of x component of image (Default: 20) 
-    window_sizey int
+    window_sizey: int
         size of y component of image (Default: 20) 
     Returns
     -------
@@ -79,13 +79,23 @@ def load_image(img=None, window_sizex=20, window_sizey=20):
             pass
     except TypeError:
         pass
-   
 
-    
-    
+
 def check_window_size(win_sizex, win_sizey):
     '''
     checks window size choosen by user and makes sure it is less than 40x40 pixels 
+
+    Parameters
+    ----------
+    win_sizex: int
+        size of window (x component)
+    win_sizey: int
+        size of window (y component)
+    Returns
+    -------
+    win_sizex, win_sizey: tuple (int)
+        returns back the window sizes (x and y) if not too big, 
+        or returns max size, if too big
     '''
     if win_sizex > 40: 
         win_sizex = 40
@@ -98,9 +108,20 @@ def check_window_size(win_sizex, win_sizey):
     
 def check_image_loaded(image=None): 
     '''
-    checks that the image needed to load is loaded properly, if no image is given, it returns back a random preset image 
-
+    checks that the image needed to load is loaded properly, if no 
+    image is given, it returns back a random preset image 
+    
+    Parameters
+    ----------
+    image: str
+        path to image (default = None) 
+    Returns
+    -------
+    bool, img : bool, array
+        returns back image and a boolean, True if the user had given an image and
+        it was loaded in properly, False if no image provided and preset image was used
     '''
+
     if image is None: 
         image_array = set_preset_image()
         return False, image_array
@@ -117,10 +138,20 @@ def check_image_loaded(image=None):
 def check_image_size(image): 
     '''
     checks the size of the image, the user is trying to use, if it is too big, let the user know 
+    
+    Parameters
+    ----------
+    image: array
+        image array
+    Returns
+    -------
+    image: array
+        returns back image if not too big, if image too big, does not return back anything
     '''
     if (np.array(image).shape[0] >= 500) and (np.array(image).shape[1] >= 500):
         print("Image is too Big! Please restrict image to 500x500 pixels")
         pass
+    
     elif (np.array(image).shape[0] <= 3) and (np.array(image).shape[1] <= 3):
         print("Image is too Small! Please add an image greater than 3x3 pixels")
         pass
@@ -133,6 +164,12 @@ def check_image_size(image):
 def set_preset_image(): 
     '''
     selects a random image from a preset array dictionary
+    
+    Returns
+    -------
+    image: array
+        returns back a random image from the already preset images 
+    
     '''
 
     images = {"GALAXY": np.array(img2bw(np.array(Image.open("images/galaxy.png").resize((15, 15))))),
@@ -150,6 +187,18 @@ def set_preset_image():
 def plot_grid(length, width): 
     '''
     plot the grid with the coordinates of each box depending on the size, specifed by the function
+    
+    Parameters
+    ----------
+    length: int
+        length of image to plot
+    width: int
+        width of image to plot
+        
+    Returns
+    -------
+    fig, ax: figure and subplot
+        returns back an image with the size given by the length and width, with equal spaced grid
     '''
     fig, ax = plt.subplots(figsize=(length, width), dpi=50)
     ax.set_xlim(0, length)
@@ -171,12 +220,34 @@ def plot_grid(length, width):
 def consecutive(data, stepsize=1):
     '''
     finds consectuive numbers in an array 
+    
+    Parameters
+    ----------
+    data: array
+        array of numbers 
+    stepsize: int
+        step size to loop through array
+        
+    Returns
+    -------
+    array: array
+        array showing where consecutive numbers are in given array
     '''
     return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
 def get_hint(array): 
     '''
     given an array of 1s and 0s, it returns back grouped hints of where the ones are and how they are grouped 
+    
+    Parameters
+    ----------
+    array: array
+        array of 0s and 1s
+        
+    Returns
+    -------
+    hints: array
+        returns back picross hints (number of groups and size of groups) given an array
     '''
     one, = np.where(array == 1)
     np.split(array, np.where(np.diff(array) != 1)[0]+1)
@@ -186,6 +257,20 @@ def get_hint(array):
 def plot_hints(img_array, length, width): 
     '''
     puts hints at the edges of the image 
+    
+    Parameters
+    ----------
+    img_array: array
+        array containing the image
+    length: int
+        length of grid to plot image
+    width: int
+        width of grid to plot image
+        
+    Returns
+    -------
+    fig, ax: figure and subplot
+        figure with grid plotted by the size given and hints plotted on the sides 
     '''
     
     fig, ax = plot_grid(length, width)
@@ -197,7 +282,8 @@ def plot_hints(img_array, length, width):
         row_hint.append(get_hint(img_array[i]))
         column_hint.append(get_hint(img_array.T[i]))
     
-    column_hint = [list(reversed(col)) for col in column_hint]
+    column_hint = [list(reversed(col)) for col in column_hint] # column hints have to be plotted vertically
+    
     # plot rows hints 
     for i in np.arange(1, length+1):
         ax.text(-0.5*len(row_hint[i-1]), i-0.5, ''.join(str(row_hint[i-1]).strip("[]")), fontsize=20)
